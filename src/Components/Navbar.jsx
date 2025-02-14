ccf394cbeceb951218a2666acb9f47f134241e90
Mateennazir <mateennazir0@gmail.com>
2025-02-14 22:29:35 +0530

    Completed the Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,28 +3,39 @@ import { FaSearch, FaShoppingCart, FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
-    <nav className="bg-amber-200">
-      <div className="container mx-auto px-4 md:px-16lg:px-24 flex justify-between items-center">
-        <div>
+    <nav className="bg-amber-300 shadow-md">
+      <div className="container mx-auto px-4 md:px-16 lg:py-4 flex justify-between items-center">
+        <div className="text-lg font-bold">
           <Link to="/">Harmoni</Link>
         </div>
-        <div>
+        <div className="relative flex-1 mx-4">
           <form>
-            <input type="text" placeholder="Search Product" />
-            <FaSearch></FaSearch>
+            <input
+              type="text"
+              placeholder="What are you looking for?"
+              className="w-full border rounded-xl py-2 px-4 bg-amber-50"
+            />
+            <FaSearch className="absolute top-3 right-3 text-black"></FaSearch>
           </form>
         </div>
 
-        <div>
+        <div className="flex items-center space-x-4">
           <Link to="/cart">
-            <FaShoppingCart />
+            <FaShoppingCart className="text-lg" />
           </Link>
 
-          <button>Login| Register</button>
-          <button>
+          <button className="hidden md:block"> Login | Register</button>
+          <button className="block md:hidden">
             <FaUser />
           </button>
         </div>
+      </div>
+
+      <div className="flex items-center justify-center space-x-10 py-4 text-sm font-bold">
+        <Link className="hover:underline">Home</Link>
+        <Link className="hover:underline">Shop</Link>
+        <Link className="hover:underline">Contact</Link>
+        <Link className="hover:underline">About</Link>
       </div>
     </nav>
   );
